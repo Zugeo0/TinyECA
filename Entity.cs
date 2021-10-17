540cb2106532ae1221d22a000a22c7892f7137fd
Axel Snaer <axelatlason2003@gmail.com>
2021-10-17 05:38:28 +0000

    Made CallComponentMethod public in the Entity class

--- Entity.cs
+++ Entity.cs
@@ -50,21 +50,21 @@ namespace TinyECA
             }
         }
 
-        internal void CallComponentMethod<T>(string methodName, object[] parameters) where T : EntityComponent
+        public void CallComponentMethod<T>(string methodName, object[] parameters) where T : EntityComponent
         {
             T component = (T)components.Find(c => c.GetType() == typeof(T));
             if (component is null) return;
             CallComponentMethod(component, methodName, parameters);
         }
         
-        internal void CallComponentMethod(EntityComponent component, string methodName, object[] parameters)
+        public void CallComponentMethod(EntityComponent component, string methodName, object[] parameters)
         {
             MethodInfo info = component.GetType().GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
             info?.Invoke(component, parameters);
         }
         
-        internal void CallComponentMethod<T>(string methodName) where T : EntityComponent => CallComponentMethod<T>(methodName, new object[] { });
-        //internal void CallComponentMethod(EntityComponent component, string methodName) => CallComponentMethod(component, methodName, new object[] { });
+        public void CallComponentMethod<T>(string methodName) where T : EntityComponent => CallComponentMethod<T>(methodName, new object[] { });
+        public void CallComponentMethod(EntityComponent component, string methodName) => CallComponentMethod(component, methodName, new object[] { });
         
         private List<EntityComponent> components = new List<EntityComponent>();
         
